147f50b3a2bcd09917e23b59bccf48315ecafca3
Yash Bonde <bonde.yash97@gmail.com>
2018-11-17 10:04:26 +0530

    Create test.py

---
+++ rectangle/test.py
@@ -0,0 +1,11 @@
+# this is the test file to import and run code
+
+import rectangle_wrapper # custom package 
+
+# define the object
+rect = rectangle_wrapper.PyRectangle(2, 4, 6, 8) 
+
+# pring values
+print(rect.getLength())
+print(rect.getHeight())
+print(rect.getArea())
